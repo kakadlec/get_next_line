c9a009350fcadaf2e15aef10c99987ad39e1031b
Karel Kadlec <kakadlec@gmail.com>
2021-06-13 18:08:59 -0300

    initial files

--- get_next_line.h
+++ get_next_line.h
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkadlec <kkadlec@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/12 15:34:33 by kakadlec          #+#    #+#             */
-/*   Updated: 2021/06/13 18:07:27 by kkadlec          ###   ########.fr       */
+/*   Created: 2021/06/13 18:02:24 by kkadlec           #+#    #+#             */
+/*   Updated: 2021/06/13 18:07:03 by kkadlec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int get_next_line(int fd, char **line)
-{
+int get_next_line(int fd, char **line);
 
-}
+#endif
